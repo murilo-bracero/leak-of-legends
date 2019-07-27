f2ca10ad0a57df9123dda880886ab3bea21fc5cd
Murilo Amais Bracero <murilobracero@gmail.com>
2019-07-27 01:59:16 -0300

    update

--- src/pages/unknow/index.js
+++ src/pages/unknow/index.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
+import sadPoro from '../../assets/poro-sad.png'
+import './styles.css';
 
 class Unknow extends Component{
     render(){
         return(
             <div className="unknow">
-                <h1>EeRrOoR 404</h1>
-                <p>Well, looks like you've tried to access a page that no longer exists in our website. We sorry :(</p>
+                <section>
+                    <h1>EeRrOoR 404</h1>
+                    <p>Well, looks like you've tried to access a page that no longer exists in our website. 
+                       We sorry :(</p>
+                    <img src={sadPoro} alt="sad poro"/>
+                </section>
             </div>
         );
     }
